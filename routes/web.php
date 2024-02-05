1e4378378c10c76a56b67819fd1a9e685976769a
Carlos Yuri Figueiredo <medeirosfigueiredoc@gmail.com>
2024-02-05 13:37:50 -0300

    Session 12 - Finally project Super Gestão

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,38 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\ContatoController;
 use App\Http\Controllers\FornecedorController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\TesteController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', [PrincipalController::class, 'principal'])->name('site.index');
-
-Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
-Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
+Route::get('/', [PrincipalController::class, 'principal'])
+    ->name('site.index')
+    ->middleware('log.acesso');
 
 Route::get('/sobre-nos', [SobreNosController::class, 'sobreNos'])->name('site.sobrenos');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 
-Route::prefix('/app')->group(function () {
+Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
 
-    Route::get('/clientes', function () {
-        return 'clientes';
-    })->name('app.clientes');
+Route::get('/login/{erro?}', [LoginController::class, 'index'])->name('site.login');
+Route::post('/login', [LoginController::class, 'autenticar'])->name('site.login');
 
-    Route::get('/fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
+Route::middleware('autenticacao:padrao,visitante,p3,p4')->prefix('/app')->group(function () {
 
-    Route::get('/produtos', function () {
-        return 'produtos';
-    })->name('app.produtos');
+    Route::get('/home', [HomeController::class, 'index'])->name('app.home');
+    Route::get('/sair', [LoginController::class, 'sair'])->name('app.sair');
+    Route::get('/cliente', [ClienteController::class, 'index'])->name('app.cliente');
+    Route::get('/fornecedor', [FornecedorController::class, 'index'])->name('app.fornecedor');
+    Route::get('/produto', [ProdutoController::class, 'index'])->name('app.produto');
 });
+
+Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('site.teste');
 
 Route::fallback(function () {
     echo "A rota acessada não existe. <a href=" . route('site.index') . "> Clique aqui </a> para ir para a página inicial!";
